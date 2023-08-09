05889f5698586c7dbf566c60bae76b197ba7dbfc
szlab-code <shizhang.chn@gmail.com>
2023-08-09 22:48:50 +0800

    first version of complete experiment (fillers not included)

--- data_includes/main.js
+++ data_includes/main.js
@@ -3,7 +3,7 @@ PennController.ResetPrefix(null); // Shorten command names (keep this line here)
 // DebugOff()   // Uncomment this line only when you are 100% done designing your experiment
 
 // First show instructions, then experiment trials, send results and show end screen
-Sequence("instructions", "experiment", SendResults(), "end")
+Sequence("welcome", "instruction", "practice", "experiment", SendResults(), "end")
 
 // This is run at the beginning of each trial
 Header(
@@ -13,19 +13,19 @@ Header(
 .log( "id" , getVar("ID") ) // Add the ID to all trials' results lines
 
 // Instructions
-newTrial("instruction",
+newTrial("welcome",
     // Automatically print all Text and Button elements, centered
-    defaultText.center().print()
+    defaultText.center().print().css("margin", "1em")
     ,
-    defaultButton.center().print()
+    defaultButton.center().print().css("margin", "1em")
     ,
     newText("欢迎参加该阅读实验。")
     ,
     newText("在该实验中，您将会阅读一系列句子。")
     ,
-    newText("在阅读完句子后，您需要回答和句子内容有关的问题。")
+    newText("阅读完句子后，您需要回答和句子内容有关的问题。")
     ,
-    newText("请在下方输入您的ID，并点击<button>开始</button>按钮以开始实验。")
+    newText("请在下方输入您的ID，并点击<b>开始</b>按钮以开始练习。")
     ,
     newTextInput("inputID", "")
         .center()
@@ -43,56 +43,130 @@ newTrial("instruction",
 )
 
 // First, practice trial
-newTrial( "experiment",
+Template("practice.csv", row =>
+    newTrial( "instruction",
+        // Automatically print all Text and Button elements, centered
+        defaultText.center().print().css("margin", "1em")
+        ,
+        defaultButton.center().print().css("margin", "1em")
+        ,
+        newText("instruction_1", "请点击<b>开始阅读</b>以开始逐词阅读句子。当您阅读完一个词后，请按<b>空格键</b>，下一个词会自动出现。")
+            .print()
+        ,
+        newButton("开始阅读")
+            .print()
+            .wait()
+            .remove()
+        ,
+        getText("instruction_1")
+            .remove()
+        ,
+        // We use the native-Ibex "DashedSentence" controller
+        // Documentation at: https://github.com/addrummond/ibex/blob/master/docs/manual.md#dashedsentence
+        newController("DashedSentence", {
+            s: row.Sentence})
+            .center()
+            .print()
+            .log()      // Make sure to log the participant's progress
+            .wait()
+            .remove()
+        ,
+        newText("instruction_2", "当您阅读完句子后，请点击<b>回答问题</b>，并根据句子内容回答问题。")
+            .print()
+        ,
+        newButton("回答问题")
+            .print()
+            .wait()
+            .remove()
+        ,
+        getText("instruction_2")
+            .remove()
+        ,
+        newController("Question", {
+            q: row.Question, 
+            as: [row.Correct, row.Wrong],
+            instructions: "请根据刚刚阅读过的句子内容，判断该陈述正确或错误，并点击对应的选项。",
+            hasCorrect: true, 
+            showNumbers: false
+        })
+            .center()
+            .print()
+            .log()      // Make sure to log the participant's progress
+            .wait()
+            .remove()
+    )
+    .log("item", row.Item)
+    .log("list", row.List)
+    .log("sentence", row.Sentence)
+    .log("qType", row.QuestionType)
+    .log("correctAnswer", row.Correct)
+    .log("wrongAnswer", row.Wrong)
+    .log("VP", row.VP)
+    .log("lexicalAsp", row.LexicalAspect)
+    .log("grammaticalAsp", row.GrammaticalAspect)    
+)
+
+// Second, inform of the formal experiment
+newTrial("before_start",
     // Automatically print all Text and Button elements, centered
-    defaultText.center().print()
+    defaultText.center().print().css("margin", "1em")
     ,
-    defaultButton.center().print()
+    defaultButton.center().print().css("margin", "1em")
     ,
-    newText("instructions", "Click on the button below to start reading. Click spacebar to proceed to the next word.")
-        .print()
+    newText("您已完成练习。")
     ,
-    newButton("Start reading")
-        .print()
-        .wait()
-        .remove()
+    newText("当您准备好后，请点击<b>开始</b>按钮，以开始正式实验。")
     ,
-    getText("instructions")
-        .remove()
+    newText("请注意，在正式实验中，将不再会出现任何和操作有关的提示。")
     ,
-    // We use the native-Ibex "DashedSentence" controller
-    // Documentation at:   https://github.com/addrummond/ibex/blob/master/docs/manual.md#dashedsentence
-    newController("DashedSentence", {s : "This is an example."})
+    newButton("开始")
         .center()
-        .print()
-        .log()      // Make sure to log the participant's progress
-        .wait()
-        .remove()
-    ,
-    newButton("Next sentence please!")
         .print()
         .wait()
 )
 
-// Second, more concise experiment trial
+// Third, formal experiment trial
 Template("stimuli.csv", row =>
     newTrial( "experiment",
         // Automatically print all Text and Button elements, centered
-        defaultText.center().print()
+        defaultText.center().print().css("margin", "1em")
         ,
-        defaultButton.center().print()
+        defaultButton.center().print().css("margin", "1em")
         ,
-        newController("DashedSentence", {s : row.Sentence})
+        newController("DashedSentence", {
+            s: row.Sentence})
             .center()
             .print()
             .log()
             .wait()
             .remove()
         ,
-        newButton("I'm done")
+        newButton("回答问题")
             .print()
             .wait()
+            .remove()
+        ,
+        newController("Question", {
+            q: row.Question, 
+            as: [row.Correct, row.Wrong],
+            hasCorrect: true, 
+            showNumbers: false
+        })
+            .center()
+            .print()
+            .log()      // Make sure to log the participant's progress
+            .wait()
+            .remove()
     )
+    .log("item", row.Item)
+    .log("list", row.List)
+    .log("sentence", row.Sentence)
+    .log("qType", row.QuestionType)
+    .log("correctAnswer", row.Correct)
+    .log("wrongAnswer", row.Wrong)
+    .log("VP", row.VP)
+    .log("lexicalAsp", row.LexicalAspect)
+    .log("grammaticalAsp", row.GrammaticalAspect)
 )
 
 
